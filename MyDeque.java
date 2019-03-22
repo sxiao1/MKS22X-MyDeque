deb71f9ad58a8634884080e13d5e2d6d9d7984a7
sxiao1 <sxiao1@stuy.edu>
2019-03-22 10:08:38 -0400

    added new functions

--- MyDeque.java
+++ MyDeque.java
@@ -27,10 +27,35 @@ public class MyDeque<E>{
     return newstr;
   }
   public void addFirst(E element){
-    
+    @SuppressWarnings("unchecked")
+    E[] temp = (E[]) new Object[data.length + 1];
+    temp[0] = element;
+    for(int i = 1; i < temp.length; i++){
+      temp[i] = data[i-1];
+    }
+    for(int i = 0; i < data.length; i++){
+      data[i] = temp[i];
+    }
+    start = (int)data[0];
   }
   public void addLast(E element){
-
+    if(data[data.length -1] != null ){
+      expand(data);
+    }
+    data[end + 1] = element;
+    end ++;
+  }
+  public void expand(E[]data){
+    @SuppressWarnings("unchecked")
+    E[] temp = (E[])new Object[data.length * 2];
+    for(int i = 0; i < data.length; i ++){
+      temp[i] = data[i];
+    }
+    /*@SuppressWarnings("unchecked")
+    data = temp;
+    for(int i = 0; i < temp.length; i++){
+      data[i] = temp[i];
+    }*/
   }
   public E removeFirst(){
     start ++;
