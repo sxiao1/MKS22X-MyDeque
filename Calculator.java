c83e1dab5bd57a3dde37952488dcc2f713c628bd
sxiao1 <sxiao1@stuy.edu>
2019-03-25 10:17:54 -0400

    working on code

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator{
+    /*Evaluate a postfix expression stored in s.
+     *Assume valid postfix notation, separated by spaces.
+     */
+    /*public static double eval(String s){
+      s = s.split(" ");
+      int[] nums = new int[s.length];
+      for(int i = 0; i < nums.length; i++){
+        if(s.charAt(i).equals("+") ||s.charAt(i).equals("-") || s.charAt(i).equals("*") || s.charAt(i).equals("/") || s.charAt(i).equals("%")){
+          if((s.charAt(i)).equals("+")){
+            nums[i] = 1;
+          }
+
+        }
+      }
+      return 0.0;
+    }*/
+}
