0233b699737ec6087253431be51e7c31352227ee
sxiao1 <sxiao1@stuy.edu>
2019-03-26 10:02:00 -0400

    tested on printing out the array

--- Calculator.java
+++ Calculator.java
@@ -5,11 +5,15 @@ public class Calculator{
     public static double eval(String s){
       String[] newArray = s.split(" ");
       MyDeque array = new MyDeque(newArray.length);
-      System.out.println(newArray);
+      System.out.println(toString(newArray));
       return 0.0;
     }
-    public static void main(String[] args){
-      Calculator test = new Calculator();
-      System.out.println(test.eval("1 3 + 4 5 9 -"));
+    public static String toString(String[] array){
+      String newstr = "";
+      for(int i = 0; i < array.length; i++){
+        newstr += array[i];
+      }
+      return newstr;
     }
+
 }
---
+++ testing.java
@@ -0,0 +1,5 @@
+public class testing{
+  public static void main(String[] args){
+    System.out.println(Calculator.eval("1 3 + 4 5 9 -"));
+  }
+}
